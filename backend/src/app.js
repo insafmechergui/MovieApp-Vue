475067280410b95419f54086e6d3ca022a5b1cb2
Insaf <insaf_mechergui@outlook.com>
2021-01-26 14:29:24 +0100

    trying with back

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var dotenv_1 = __importDefault(require("dotenv"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
 var app = express_1.default();
-dotenv_1.default.config();
-// const PORT = process.env.SERVER_PORT || 5000;
-var PORT = 5000;
-app.get('/', function (req, res) {
-    res.send('hello world');
+app.use(body_parser_1.default.json());
+app.use(cors_1.default());
+// dotenv.config();
+// var uri = process.env.SERVER_PORT 
+// mongoose.connect(uri, 
+//   {  useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }  
+// );
+var PORT = process.env.PORT || 5000;
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
+app.get('/status', function (req, res) {
+    res.send({
+        message: 'hellohhhh'
+    });
 });
 app.listen(PORT, function () { console.log("server is listening on " + PORT); });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from "dotenv";
 
 const app = express();
-dotenv.config()
+app.use(bodyParser.json());
+app.use(cors());
 
-// const PORT = process.env.SERVER_PORT || 5000;
-const PORT =  5000;
+// dotenv.config();
+// var uri = process.env.SERVER_PORT 
+// mongoose.connect(uri, 
+//   {  useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }  
+// );
+  const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
+app.get('/status', (req, res) => {
+  res.send({
+    message:'hellohhhh'
+  });
 });
 app.listen(PORT, ()=>{ console.log(`server is listening on ${PORT}`)});
