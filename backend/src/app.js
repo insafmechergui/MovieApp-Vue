2d4ed96a6b07a85bd74f8136b5dec27d4636ac53
Insaf <insaf_mechergui@outlook.com>
2021-01-26 13:00:39 +0100

    init

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
 var app = express_1.default();
-var PORT = 3000;
+dotenv_1.default.config();
+// const PORT = process.env.SERVER_PORT || 5000;
+var PORT = 5000;
 app.get('/', function (req, res) {
     res.send('hello world');
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import dotenv from "dotenv";
 
 const app = express();
 dotenv.config()
 
-const PORT = process.env.SERVER_PORT;
+// const PORT = process.env.SERVER_PORT || 5000;
+const PORT =  5000;
 
 app.get('/', (req, res) => {
   res.send('hello world');
